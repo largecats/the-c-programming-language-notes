8ce8c616a16c5659f1bfb617027de150eaa49d73
largecats <xiaolf@seagroup.com>
2020-06-19 17:56:02 +0800

    updated

--- chapter5/scratch.c
+++ chapter5/scratch.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include "../helper_functions.h"
 
 int main() {
-    printf("%d", strcmp("a", "b"));    
+    print_variable('A');
+    print_variable('a');
+    print_variable('Z');
+    printf("%f\n", atof("A"));
+    printf("%f\n", atof("a"));
 
     return 0;
 }
