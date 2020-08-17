7a0ff59707037f823b195179c5650d85f0389485
largecats <xiaolf@xiaolf.sg.corp.seagroup.com>
2020-08-17 18:23:55 +0800

    updated

--- chapter5/5.12/exercise_5-18/main.c
+++ chapter5/5.12/exercise_5-18/main.c
@@ -9,7 +9,7 @@ According to https://stackoverflow.com/questions/20170068/kr-c-chapter-5-dcl-iss
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include "../helper_functions.h"
+#include "../../../helper_functions.h"
 
 #define MAXTOKEN 100
 
@@ -26,7 +26,7 @@ char token[MAXTOKEN]; /* last toekn string */
 char name[MAXTOKEN]; /* identifier name */
 char datatype[MAXTOKEN]; /* data type = char, int, etc */
 char out[1000]; /* output string */
-int prevtoken = NO;
+int prevtoken;
 
 int main() {
     /* convert declaration to words */
@@ -43,7 +43,9 @@ int main() {
 }
 
 /*
-$ chapter5/result.out
+$ gcc chapter5/5.12/exercise_5-18/main.c chapter5/5.12/exercise_5-18/gettoken.c helper_functions.c -o chapter5/5.12/exercise_5-18/result.out
+
+$ chapter5/5.12/exercise_5-18/result.out
 token =
 tokentype = 0
 int (*)[13]
@@ -86,60 +88,6 @@ daytab:  array[13] of pointer to int // missing ) does not affect output and is 
 token = [13]
 tokentype =
 */
-
-int gettoken(void) {
-    /* return the type of the next token, write token value to variable token, and write token type to variable tokentype */
-    int c, getch(void);
-    void ungetch(int);
-    char *p = token;
-
-    print_string(token);
-    if (tokentype == NAME || tokentype == PARENS || tokentype == BRACKETS) {
-        print_variable(tokentype);
-    }
-    else {
-        print_char(tokentype);
-    }
-    
-    /* handle error by returning the last tokentype and move on */
-    if (prevtoken == YES) {
-        prevtoken = NO;
-        return tokentype;
-    }
-
-    while ((c = getch()) == ' ' || c == '\t') {
-        ; /* skip blanks and tabs */
-    }
-    if (c == '(') {
-        /* peek at the next character */
-        if ((c = getch()) == ')') { /* if the next character is closing paren */
-            strcpy(token, "()"); /* store token '()' in variable token */
-            return tokentype = PARENS;
-        }
-        else {
-            ungetch(c); /* put back the next character to input buffer*/
-            return tokentype = '(';
-        }
-    }
-    else if (c == '[') {
-        for (*p++ = c; (*p++ = getch()) != ']'; ) {
-            ; /* read characters enclosed by [] */
-        }
-        *p = '\0';
-        return tokentype = BRACKETS;
-    }
-    else if (isalpha(c)) {
-        /* name starts with letter and may contain numbers */
-        for (*p++ = c; isalnum(c = getch());) {
-            *p++ = c; /* read name */
-        }
-        *p = '\0';
-        ungetch(c);
-        return tokentype = NAME;
-    } else {
-        return tokentype = c;
-    }
-}
 
 void errmsg(char *msg)
 {
