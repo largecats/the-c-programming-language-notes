8dba8ce77d8fe721ab200315cc91a68e6f998a19
largecats <xiaolf@seagroup.com>
2020-09-01 18:32:57 +0800

    updated

---
+++ chapter8/8.3/exercise_8-1/main.c
@@ -0,0 +1,4 @@
+/*
+Exercise 8-1. Rewrite the program cat from Chapter 7 using read, write, open, and close instead of their standard library
+equivalents. Perform experiments to determine the relative speeds of the two versions.
+*/
